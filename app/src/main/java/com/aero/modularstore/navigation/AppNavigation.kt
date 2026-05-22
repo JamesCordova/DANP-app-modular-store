c92482c772a97fb0ed0fec6ceaace769a377feee
JamesCordova <jcordovas@unsa.edu.pe>
2026-05-22 09:47:04 -0500

    feat: add AppNavigation component for managing app navigation

---
+++ app/src/main/java/com/aero/modularstore/navigation/AppNavigation.kt
@@ -0,0 +1,38 @@
+package com.aero.modularstore.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.aero.modularstore.ui.screens.DetailScreen
+import com.aero.modularstore.ui.screens.HomeScreen
+
+@Composable
+fun AppNavigation(
+    onThemeChange: (String) -> Unit
+) {
+    val navController = rememberNavController()
+    NavHost(
+        navController = navController,
+        startDestination = "home"
+    ) {
+        composable("home") {
+            HomeScreen(
+                navController,
+                onThemeChange
+            )
+        }
+        composable(
+            "detail/{name}/{price}"
+        ) { backStack ->
+            val name =
+                backStack.arguments?.getString("name") ?: ""
+            val price =
+                backStack.arguments?.getString("price") ?: ""
+            DetailScreen(
+                name,
+                price
+            )
+        }
+    }
+}
